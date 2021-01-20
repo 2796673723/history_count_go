47b6e655e9193539656b72620f563c6492e5041d
luoluo <2797763723@qq.com>
2021-01-20 13:58:18 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module history
+
+go 1.15
+
+require (
+	github.com/mattn/go-sqlite3 v1.14.6
+	github.com/wcharczuk/go-chart/v2 v2.1.0
+	golang.org/x/image v0.0.0-20201208152932-35266b937fa6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/wcharczuk/go-chart/v2"
+)
+
+type website struct {
+	Name  string `json:"name"`
+	Count int    `json:"count"`
+}
+
+func main() {
+	db, err := sql.Open("sqlite3", "./History")
+	if err != nil {
+		fmt.Println("数据库连接失败")
+		return
+	}
+
+	rows, err := db.Query("select url from urls")
+	if err != nil {
+		fmt.Println("查询失败")
+		return
+	}
+
+	websiteMap := make(map[string]int)
+	for rows.Next() {
+		var url string
+		rows.Scan(&url)
+		website := strings.Split(url, "/")[2]
+		count, ok := websiteMap[website]
+		if ok {
+			websiteMap[website] = count + 1
+		} else {
+			websiteMap[website] = 1
+		}
+	}
+
+	var websiteList []website
+	for key, value := range websiteMap {
+		websiteList = append(websiteList, website{
+			Name:  key,
+			Count: value,
+		})
+	}
+	sort.Slice(websiteList, func(i, j int) bool { return websiteList[i].Count > websiteList[j].Count })
+
+	historyJSON, err := json.Marshal(websiteList)
+	if err != nil {
+		fmt.Println("json 转换错误")
+	} else {
+		jsonFile, err := os.Create("history.json")
+		if err != nil {
+			fmt.Println("创建json错误")
+		} else {
+			jsonFile.Write(historyJSON)
+			jsonFile.Close()
+		}
+	}
+
+	PieData := []chart.Value{}
+	for i := 0; i < 15; i++ {
+		PieData = append(PieData, chart.Value{
+			Label: websiteList[i].Name,
+			Value: float64(websiteList[i].Count),
+		})
+	}
+
+	pie := chart.DonutChart{
+		Width:  2000,
+		Height: 2000,
+		DPI:    100,
+		Values: PieData,
+	}
+
+	pieChart, err := os.Create("history.png")
+	if err != nil {
+		fmt.Println("输出图片错误")
+	} else {
+		pie.Render(chart.PNG, pieChart)
+		pieChart.Close()
+	}
+
+}
